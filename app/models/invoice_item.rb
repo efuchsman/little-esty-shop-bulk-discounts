406bfd42e847f45100a7f52f2d8b77a4b20681ed
Eli Fuchsman <efuchsman@gmail.com>
2022-11-11 11:20:27 -0700

    Write method to convert invoice item unit price to dollars

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,4 +8,8 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :quantity, :unit_price, :status, :item_id, :invoice_id
 
   enum status: { pending: 0, packaged: 1, shipped: 2 }
+
+  def unit_price_to_dollars
+    (unit_price.to_f/100.00).round(2)
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :invoice_id }
   end
+
+  describe "Instance Methods" do
+
+    before :each do
+      @customer_1 = Customer.create!(first_name: 'Eli', last_name: 'Fuchsman')
+      @merchant = Merchant.create!(name: 'Test')
+      @item_1 = Item.create!(name: 'item1', description: 'desc1', unit_price: 12053, merchant_id: @merchant.id)
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 1)
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 12053,
+        status: 1)
+    end
+
+    describe "#unit_price_to_dollars" do
+      it "converts cents to dollars" do
+        expect(@ii_1.unit_price_to_dollars).to eq(120.53)
+      end
+    end
+  end
 end
